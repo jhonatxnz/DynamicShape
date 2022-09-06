332c8caca094bf679e11eb2f7728a16d955bf0f3
Jhon <jw.jhonatan1711@gmail.com>
2022-09-05 21:05:03 -0300

    metade das telas

---
+++ app/src/main/java/br/unicamp/dynamicshape/Objetivo.java
@@ -0,0 +1,13 @@
+package br.unicamp.dynamicshape;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Objetivo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_objetivo);
+    }
+}
---
+++ app/src/main/java/br/unicamp/dynamicshape/PesoAltura.java
@@ -0,0 +1,14 @@
+package br.unicamp.dynamicshape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PesoAltura extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_peso_altura);
+    }
+}
